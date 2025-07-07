4af4f1b0b68748fa9bcdc50dad41e5eae2d6f9d6
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2025-07-07 22:35:40 +0900

    feat: 비관적 락을 적용한 TimeSale 조회 메서드(findByIdWithPessimisticLock) 추가

--- src/main/kotlin/com/study/timesale/repository/TimeSaleRepository.kt
+++ src/main/kotlin/com/study/timesale/repository/TimeSaleRepository.kt
@@ -2,16 +2,22 @@ package com.study.timesale.repository
 
 import com.study.timesale.domain.TimeSale
 import com.study.timesale.domain.TimeSaleStatus
+import jakarta.persistence.LockModeType
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 import kotlin.jvm.optionals.getOrNull
 
 fun TimeSaleRepository.getTimeSaleById(id: Long): TimeSale =
-    this.findById(id).getOrNull() ?: throw NoSuchElementException("Product not found")
+    this.findById(id).getOrNull() ?: throw NoSuchElementException("TimeSale not found")
+
+fun TimeSaleRepository.getTimeSaleByIdWithPessimisticLock(id: Long): TimeSale =
+    this.findByIdWithPessimisticLock(id) ?: throw NoSuchElementException("Product not found")
 
 @Repository
 interface TimeSaleRepository : JpaRepository<TimeSale, Long> {
@@ -21,4 +27,8 @@ interface TimeSaleRepository : JpaRepository<TimeSale, Long> {
         status: TimeSaleStatus,
         pageable: Pageable,
     ): Page<TimeSale>
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ts from TimeSale ts where ts.id = :id")
+    fun findByIdWithPessimisticLock(@Param("id") id: Long): TimeSale?
 }
