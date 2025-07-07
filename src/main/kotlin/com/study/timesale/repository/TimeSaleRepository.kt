f422575aee887282ca5c3c2187148a2e632c5fbe
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2025-07-07 22:23:36 +0900

    현재 진행중인 타임세일 조회 쿼리 메서드 작성

--- src/main/kotlin/com/study/timesale/repository/TimeSaleRepository.kt
+++ src/main/kotlin/com/study/timesale/repository/TimeSaleRepository.kt
@@ -1,12 +1,24 @@
 package com.study.timesale.repository
 
 import com.study.timesale.domain.TimeSale
+import com.study.timesale.domain.TimeSaleStatus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 import kotlin.jvm.optionals.getOrNull
 
 fun TimeSaleRepository.getTimeSaleById(id: Long): TimeSale =
     this.findById(id).getOrNull() ?: throw NoSuchElementException("Product not found")
 
 @Repository
-interface TimeSaleRepository : JpaRepository<TimeSale, Long>
+interface TimeSaleRepository : JpaRepository<TimeSale, Long> {
+    @Query("select ts from TimeSale ts where ts.startAt <= :now and ts.endAt >= :now and ts.status = :status")
+    fun findAllByStartAtBeforeAndEndAtAfterAndStatus(
+        now: LocalDateTime,
+        status: TimeSaleStatus,
+        pageable: Pageable,
+    ): Page<TimeSale>
+}
