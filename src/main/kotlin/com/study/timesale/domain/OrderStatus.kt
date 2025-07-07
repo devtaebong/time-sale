c5737e434c1643ff67308bd3bec7c4f3268dd8ad
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2025-07-07 21:47:14 +0900

    주문 도메인 추가

---
+++ src/main/kotlin/com/study/timesale/domain/OrderStatus.kt
@@ -0,0 +1,5 @@
+package com.study.timesale.domain
+
+enum class OrderStatus {
+    PENDING, COMPLETED, FAILED
+}
---
+++ src/main/kotlin/com/study/timesale/domain/TimeSaleOrder.kt
@@ -0,0 +1,49 @@
+package com.study.timesale.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "time_sale_orders")
+@EntityListeners(AuditingEntityListener::class)
+class TimeSaleOrder(
+    @Column(nullable = false)
+    val userId: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "time_sale_id", nullable = false)
+    val timeSale: TimeSale,
+
+    @Column(nullable = false)
+    val quantity: Int,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var orderStatus: OrderStatus = OrderStatus.PENDING,
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    val createdAt: LocalDateTime,
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    val updatedAt: LocalDateTime,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+)
