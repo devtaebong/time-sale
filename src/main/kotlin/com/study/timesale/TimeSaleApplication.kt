17d586ed52c415174fde1c38c2d52f0f4149f390
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2025-07-07 21:19:14 +0900

    Init

---
+++ src/main/kotlin/com/study/timesale/TimeSaleApplication.kt
@@ -0,0 +1,11 @@
+package com.study.timesale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeSaleApplication
+
+fun main(args: Array<String>) {
+    runApplication<TimeSaleApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/timesale/TimeSaleApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.study.timesale
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeSaleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
